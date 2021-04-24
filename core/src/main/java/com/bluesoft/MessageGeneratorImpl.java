aedc6fdb1c61ea00b9c550276f0f3d9a6a2da932
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-04-24 16:08:34 +0200

    MessageGenerator = adding functionality for getMainMessage and getResultMessage.

--- core/src/main/java/com/bluesoft/MessageGeneratorImpl.java
+++ core/src/main/java/com/bluesoft/MessageGeneratorImpl.java
@@ -22,11 +22,27 @@ public class MessageGeneratorImpl implements MessageGenerator {
 
     @Override
     public String getMainMessage() {
-        return "Calling getMainMessage";
+        return "Number is between "+ game.getSmallest() +
+                " and " + game.getBiggest() + ". Can you guess it ?";
     }
 
     @Override
     public String getResultMessage() {
-        return "Calling getResultMessage";
+        if(game.isGameWon()){
+            return "You guessed it! The number was " + game.getNumber();
+        }else if(game.isGameLost()){
+            return "You lost. The number was " + game.getNumber();
+        }else if (!game.isValidNumberRange()){
+            return  "Invalid number range!";
+        }else if(game.getRemainingGuesses() == guessCount){
+            return "What is your first guess";
+        }else{
+            String direction = "Lower";
+
+            if(game.getGuess() < game.getNumber()){
+                direction = "Higher";
+            }
+            return direction + "! You have " + game.getRemainingGuesses() + " guess left!";
+        }
     }
 }
