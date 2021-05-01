3a9ccdaecd54d999bf36eab556c4148156318cc9
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-05-01 11:35:59 +0200

    Adding parameter to annotation about event to listen

--- console/src/main/java/com/bluesoft/console/ConsoleNumberGuess.java
+++ console/src/main/java/com/bluesoft/console/ConsoleNumberGuess.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Component;
 public class ConsoleNumberGuess {
     private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
-    @EventListener
-    public void start(ContextRefreshedEvent contextRefreshedEvent) {
+    //1. method parameter is use for spring to check what kind of event we want.
+    //2. instead we can add param to @EventListener
+    @EventListener(ContextRefreshedEvent.class)
+    public void start() {
            log.info("start() Container ready for use.");
     }
 }
