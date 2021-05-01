605242dae30c268d4b31adf923392aa7d0a53aad
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-05-01 13:44:05 +0200

    Create custom annotation - @MaxNumber and @GuessCount

--- core/src/main/java/com/bluesoft/config/GameConfig.java
+++ core/src/main/java/com/bluesoft/config/GameConfig.java
@@ -1,5 +1,7 @@
 package com.bluesoft.config;
 
+import com.bluesoft.GuessCount;
+import com.bluesoft.MaxNumber;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +11,13 @@ public class GameConfig {
   private int guessCount = 8;
 
   @Bean
+  @MaxNumber
   public int maxNumber(){
       return maxNumber;
   }
 
   @Bean
+  @GuessCount
   public int guessCount() {
       return guessCount;
   }
