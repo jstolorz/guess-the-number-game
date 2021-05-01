cf4beea2e7b731615dba6d5a20ea4af4446fba78
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-05-01 11:30:51 +0200

    Moving from implement event to annotation  @EventListener

--- console/src/main/java/com/bluesoft/console/ConsoleNumberGuess.java
+++ console/src/main/java/com/bluesoft/console/ConsoleNumberGuess.java
@@ -2,16 +2,16 @@ package com.bluesoft.console;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent> {
+public class ConsoleNumberGuess {
     private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-           log.info("Container ready for use.");
+    @EventListener
+    public void start(ContextRefreshedEvent contextRefreshedEvent) {
+           log.info("start() Container ready for use.");
     }
 }
