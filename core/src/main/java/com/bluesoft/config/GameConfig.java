4fda6b1afded99afadb6bc3a6f04fe374df1e950
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-05-02 15:27:15 +0200

    Adding properties file and using @PropertySource and @Value

--- core/src/main/java/com/bluesoft/config/GameConfig.java
+++ core/src/main/java/com/bluesoft/config/GameConfig.java
@@ -2,13 +2,20 @@ package com.bluesoft.config;
 
 import com.bluesoft.GuessCount;
 import com.bluesoft.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
-  private int maxNumber = 100;
-  private int guessCount = 8;
+
+  @Value("${game.maxNumber}")
+  private int maxNumber;
+
+  @Value("${game.guessCount}")
+  private int guessCount;
 
   @Bean
   @MaxNumber
