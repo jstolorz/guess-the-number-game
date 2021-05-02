f2c0f7e4863580c4440b10c4d0c43a7c9da84fd7
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-05-02 16:26:07 +0200

    Changing field @Autowired to constructor @Autowired and clean code

--- console/src/main/java/com/bluesoft/console/Main.java
+++ console/src/main/java/com/bluesoft/console/Main.java
@@ -1,9 +1,6 @@
 package com.bluesoft.console;
 
-import com.bluesoft.config.AppConfig;
-import com.bluesoft.Game;
-import com.bluesoft.MessageGenerator;
-import com.bluesoft.NumberGenerator;
+import com.bluesoft.config.GameConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,24 +10,9 @@ public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-
     public static void main(String[] args) {
         log.info("Guess The Number Game");
-
-        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-
-        NumberGenerator generator = context.getBean(NumberGenerator.class);
-        MessageGenerator messageGenerator = context.getBean(MessageGenerator.class);
-
-        log.info(messageGenerator.getMainMessage());
-        log.info(messageGenerator.getResultMessage());
-
-        int number = generator.next();
-
-        log.info("Number = {}",number);
-
-        Game game = context.getBean(Game.class);
-
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(GameConfig.class);
         context.close();
     }
 }
